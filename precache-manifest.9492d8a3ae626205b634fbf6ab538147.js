fa2d155d0e731d8e684c398de0306aaeb0d13a9d
Sam Case <samcase@Sams-MacBook-Pro.local>
2022-01-14 11:36:07 -0600

    Updates

---
+++ precache-manifest.9492d8a3ae626205b634fbf6ab538147.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed0e96dcc8dfcf73f0ead387dd433c6",
+    "url": "/lizad/index.html"
+  },
+  {
+    "revision": "4f1bd0f57adfbc4503ed",
+    "url": "/lizad/static/css/main.19a93d44.chunk.css"
+  },
+  {
+    "revision": "85c340a4d1ac54010302",
+    "url": "/lizad/static/js/2.7634d88e.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/lizad/static/js/2.7634d88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def0f756bdae2f10139c",
+    "url": "/lizad/static/js/3.0acd0193.chunk.js"
+  },
+  {
+    "revision": "4f1bd0f57adfbc4503ed",
+    "url": "/lizad/static/js/main.3b7496a0.chunk.js"
+  },
+  {
+    "revision": "a73d2b776c48967bd9dc",
+    "url": "/lizad/static/js/runtime-main.b04d9acc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/lizad/static/media/logo.06e73328.svg"
+  }
+]);
