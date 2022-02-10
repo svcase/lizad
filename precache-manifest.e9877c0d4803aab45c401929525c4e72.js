71db4bd3477f107119b196581f18ab6a5919ba83
Sam Case <samcase@Sams-MacBook-Pro.local>
2022-02-10 15:41:00 -0600

    Updates

---
+++ precache-manifest.e9877c0d4803aab45c401929525c4e72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4071d0ae298bd6232123315b6356bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160ba4293b9ca6c110e6",
+    "url": "/static/css/main.d44a0b3f.chunk.css"
+  },
+  {
+    "revision": "85c340a4d1ac54010302",
+    "url": "/static/js/2.7634d88e.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/static/js/2.7634d88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def0f756bdae2f10139c",
+    "url": "/static/js/3.0acd0193.chunk.js"
+  },
+  {
+    "revision": "160ba4293b9ca6c110e6",
+    "url": "/static/js/main.98720998.chunk.js"
+  },
+  {
+    "revision": "341476e249a345b7a304",
+    "url": "/static/js/runtime-main.ee0bd79b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lizad/precache-manifest.40ad11178dbbbb697e97f3a345377bd2.js"
+  "/precache-manifest.e9877c0d4803aab45c401929525c4e72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lizad/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
